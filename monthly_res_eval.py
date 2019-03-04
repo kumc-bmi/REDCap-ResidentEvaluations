72cce69f8e70c93aae3d0ee842ab4aae79fb0569
Dan Connolly <dconnolly@kumc.edu>
2019-03-04 14:11:23 -0600

    ocap refactor: access to files, clock, SMTP, HTTP

--- monthly_res_eval.py
+++ monthly_res_eval.py
@@ -1,13 +1,8 @@
-import requests
-import datetime
-import json
+from json import loads
 import calendar
-import sys
 import operator
 import csv
-import os.path
-import smtplib
-from os.path import basename
+from datetime import datetime  # for strptime
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -15,48 +10,48 @@ from email.utils import formatdate
 
 
 # Function for making api call
-def api(url, data):
-    req = requests.post(url, data)
-    return json.loads(req.content)
+def api(url, data, post):
+    req = post(url, data)
+    return loads(req.content)
 
 
 # Send email
-def send_report(from_email, to_email, file_name):
+def send_report(from_email, to_email, file_path, cwd, SMTP):
     msg = MIMEMultipart()
     msg['From'] = from_email
     msg['To'] = to_email
     msg['Date'] = formatdate(localtime=True)
-    msg['Subject'] = file_name.split(".")[0]
+    msg['Subject'] = file_path.splitext()[0]
 
     text = "Attached is a CSV document containing evaluations done on you during the last month. " \
            "\n\nThank you,\nMISupport"
 
     msg.attach(MIMEText(text))
 
-    with open(file_name, "rb") as fil:
+    with file_path.open("rb") as fil:
         part = MIMEApplication(
             fil.read(),
-            Name=basename(file_name)
+            Name=file_path.name
         )
         # After the file is closed
-    part['Content-Disposition'] = 'attachment; filename="%s"' % basename(file_name)
+    part['Content-Disposition'] = 'attachment; filename="%s"' % file_path.name
     msg.attach(part)
 
-    smtp = smtplib.SMTP('smtp.kumc.edu')
+    smtp = SMTP('smtp.kumc.edu')
     smtp.sendmail(from_email, to_email, msg.as_string())
     smtp.close()
 
 
 # function for generating CSV
-def add_to_csv(title, content, fields_to_export):
-    csv_file = "{}.csv".format(title)
+def add_to_csv(title, content, fields_to_export, cwd):
+    csv_file = cwd / "{}.csv".format(title)
 
     file_exists = False
 
-    if os.path.isfile(csv_file):
+    if csv_file.isfile():
         file_exists = True
 
-    with open(csv_file, 'a') as csvfile:
+    with csv_file.open('a') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',')
 
         # label row, only if file doesn't exist
@@ -86,16 +81,15 @@ def add_to_csv(title, content, fields_to_export):
 
 
 # Retrieve residents from whole project, who have had a surgery evaluation in the last month
-def get_residents(evals):
+def get_residents(evals, cur_date):
     resident_evals = {}
-    cur_date = datetime.datetime.now()
     days_in_month = calendar.monthrange(cur_date.year, cur_date.month)[1]
 
     # Iterate through resident evaluations
     for res_eval in evals:
         res_email = res_eval['resident_email']
         res_eval_date = res_eval['date_of_surgery']
-        datetm = datetime.datetime.strptime(res_eval_date, "%Y-%m-%d")
+        datetm = datetime.strptime(res_eval_date, "%Y-%m-%d")
 
         # If the date of the surgery being evaluated happens in the last month, add that eval to the list to be exported
         if (datetime.datetime.now() - datetm).days <= days_in_month:
@@ -105,25 +99,24 @@ def get_residents(evals):
 
 
 # Sort evaluations by resident, generate CSV, and store it in the temp directory
-def generate_reports(evals, fields_to_export, api_con):
+def generate_reports(evals, fields_to_export, api_con, cur_date, cwd, post):
     sorted_evals = sorted(evals.items(), key=operator.itemgetter(1))
     emails_and_files = {}
 
     for res_eval in sorted_evals:
-        eval = get_eval(res_eval, api_con)
+        eval = get_eval(res_eval, api_con, post)
 
-        cur_date = datetime.datetime.now()
         resident_id = res_eval[1].split("@")[0]
         title = "Evaluation_Summary_{}_{}_{}".format(cur_date.month, cur_date.year, resident_id)
 
-        csv_file = add_to_csv(title, eval, fields_to_export)
-        emails_and_files[res_eval[1]] = "{}.csv".format(title)
+        csv_file = add_to_csv(title, eval, fields_to_export, cwd)
+        emails_and_files[res_eval[1]] = cwd / "{}.csv".format(title)
 
     return emails_and_files
 
 
 # Get evaluation info via API
-def get_eval(res_eval, api_con):
+def get_eval(res_eval, api_con, post):
     data = {
         'token': api_con[1],
         'content': 'record',
@@ -138,21 +131,21 @@ def get_eval(res_eval, api_con):
         'returnFormat': 'json'
     }
 
-    return api(api_con[0], data)
+    return api(api_con[0], data, post)
 
 
 # queue emails, and delete files
-def queue_emails(from_email, emails_and_files):
+def queue_emails(from_email, emails_and_files, cwd, SMTP):
     for ef in emails_and_files.keys():
-        send_report(from_email, ef, emails_and_files[ef])
+        send_report(from_email, ef, emails_and_files[ef], cwd, SMTP)
 
     for ef in emails_and_files.keys():
-        os.remove(emails_and_files[ef])
+        emails_and_files[ef].remove()
 
 
-def main():
+def main(argv, cwd, now, post, SMTP):
     # api_con = (api_url, api_key)
-    api_con = (sys.argv[1], sys.argv[2])
+    api_con = (argv[1], argv[2])
 
     data = {
         'token': api_con[1],
@@ -174,12 +167,23 @@ def main():
 
     from_email = "misupport@kumc.edu"
 
-    evals = api(api_con[0], data)
-    resident_evals = get_residents(evals)
-    emails_and_files = generate_reports(resident_evals, fields_to_export, api_con)
-    queue_emails(from_email, emails_and_files)
+    evals = api(api_con[0], data, post)
+    cur_date = now()
+    resident_evals = get_residents(evals, cur_date)
+
+    emails_and_files = generate_reports(resident_evals, fields_to_export, api_con, cur_date, cwd, post)
+    queue_emails(from_email, emails_and_files, cwd, SMTP)
 
 
 if __name__ == "__main__":
-    main()
+    def _script():
+        import requests
+        from sys import argv
+        from smtplib import SMTP
+        from datetime import datetime
+        from pathlib import Path
 
+        cwd = Path('.')
+        main(argv, cwd, datetime.now, requests.post, SMTP)
+
+    _script()
