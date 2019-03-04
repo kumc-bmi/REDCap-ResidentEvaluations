dbfa0a9792062503f376688f40855ae106002c66
Dan Connolly <dconnolly@kumc.edu>
2019-03-04 15:18:34 -0600

    monthly_res_eval: add doctest
      - tell a brief story
        - enough MockIO to run through main()
          and see that email was sent
      - test generate_reports in detail
      - hoist fields_to_export out of main()

--- monthly_res_eval.py
+++ monthly_res_eval.py
@@ -1,8 +1,39 @@
-from json import loads
+r"""monthly_res_eval - distribute evaluation reports
+
+It's spring 2019 and resident Rich performed surgery 5 days ago:
+
+    >>> resident_email = 'rich@g.com'
+    >>> now = lambda: datetime(2019, 03, 14, 1, 1, 1)
+    >>> date_of_surgery = now() - timedelta(days=5)
+
+It's time to send evaluations for the month, so we run this script,
+giving it API access to the REDCap project:
+
+    >>> argv = 'script https://redcap/api SUPERSEKRET'.split()
+    >>> io = MockIO(now, resident_email, date_of_surgery)
+    >>> main(argv, io.cwd, now, io.post, io.SMTP)
+
+Now we can see that a CSV file got sent to Rich:
+
+    >>> for from_addr, to_addr, msg in io.smtp.sent:
+    ...     print from_addr, to_addr
+    ...     filename = msg.split('filename=')[1]
+    ...     filename = filename.split('\n')[0]
+    ...     print filename
+    misupport@kumc.edu rich@g.com
+    "Evaluation_Summary_3_2019_rich.csv"
+
+"""
+
+from json import loads, dumps
 import calendar
 import operator
 import csv
-from datetime import datetime  # for strptime
+from datetime import (
+    timedelta,
+    datetime  # for strptime
+)
+from pathlib import PosixPath
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -92,14 +123,22 @@ def get_residents(evals, cur_date):
         datetm = datetime.strptime(res_eval_date, "%Y-%m-%d")
 
         # If the date of the surgery being evaluated happens in the last month, add that eval to the list to be exported
-        if (datetime.datetime.now() - datetm).days <= days_in_month:
+        if (cur_date - datetm).days <= days_in_month:
             resident_evals[res_eval['record_id']] = res_email
 
     return resident_evals
 
 
-# Sort evaluations by resident, generate CSV, and store it in the temp directory
 def generate_reports(evals, fields_to_export, api_con, cur_date, cwd, post):
+    """Sort evaluations by resident, generate CSV, and store it in the temp directory
+
+    >>> now = lambda: datetime(2001, 1, 2, 3, 4, 5, 6)
+    >>> io = MockIO(now, 'rich@g.com', now())
+    >>> generate_reports({'resident_email': 'rich@g.com'},
+    ...                  [], ('x', 'y'), now(),
+    ...                  io.cwd, io.post)
+    {'rich@g.com': MockPath('Evaluation_Summary_1_2001_rich.csv')}
+    """
     sorted_evals = sorted(evals.items(), key=operator.itemgetter(1))
     emails_and_files = {}
 
@@ -143,6 +182,13 @@ def queue_emails(from_email, emails_and_files, cwd, SMTP):
         emails_and_files[ef].remove()
 
 
+class REProject(object):
+    fields_to_export = ('resident_email', 'resident_kumc', 'training_year', 'rotation_kumc', 'surgery', 'second_surgery', 'complexity',
+                      'pre_procedure_plan', 'steps', 'technical_performance', 'visuospatial_skills_instru',
+                      'post_procedure_plan', 'communication', 'case_performed', 'critical_maneuvers',
+                      'resident_is_able_to_safely', 'done_well', 'needs_improvement', 'real_time_eval')
+
+
 def main(argv, cwd, now, post, SMTP):
     # api_con = (api_url, api_key)
     api_con = (argv[1], argv[2])
@@ -160,19 +206,69 @@ def main(argv, cwd, now, post, SMTP):
         'returnFormat': 'json'
     }
 
-    fields_to_export = ('resident_email', 'resident_kumc', 'training_year', 'rotation_kumc', 'surgery', 'second_surgery', 'complexity',
-                      'pre_procedure_plan', 'steps', 'technical_performance', 'visuospatial_skills_instru',
-                      'post_procedure_plan', 'communication', 'case_performed', 'critical_maneuvers',
-                      'resident_is_able_to_safely', 'done_well', 'needs_improvement', 'real_time_eval')
-
     from_email = "misupport@kumc.edu"
 
     evals = api(api_con[0], data, post)
     cur_date = now()
     resident_evals = get_residents(evals, cur_date)
 
-    emails_and_files = generate_reports(resident_evals, fields_to_export, api_con, cur_date, cwd, post)
+    emails_and_files = generate_reports(resident_evals, REProject.fields_to_export, api_con, cur_date, cwd, post)
     queue_emails(from_email, emails_and_files, cwd, SMTP)
+
+
+class MockIO(object):
+    def __init__(self, now, resident_email, date_of_surgery):
+        fields = {f: 'junk'
+                  for f in REProject.fields_to_export}
+        self.eval1 = dict(
+            fields,
+            record_id='123',
+            resident_email=resident_email,
+            date_of_surgery=date_of_surgery.strftime("%Y-%m-%d"))
+
+        self.cwd = MockPath('.')
+        self.cur_date = now()
+        self.smtp = None
+
+    def __repr__(self):
+        return 'MockIO(%s)' % self.cur_date.strftime("%Y-%m-%d")
+
+    def post(self, url, data):
+        return self
+
+    @property
+    def content(self):
+        return dumps([self.eval1])
+
+    def SMTP(self, server):
+        self.smtp = MockSMTP(server)
+        return self.smtp
+
+
+class MockSMTP(object):
+    def __init__(self, server):
+        self.sent = []
+
+    def sendmail(self, from_email, to_email, msg):
+        self.sent.append((from_email, to_email, msg))
+
+    def close(self):
+        pass
+
+
+class MockPath(PosixPath):
+    def isfile(self):
+        return False
+
+    def open(self, mode='r'):
+        from io import BytesIO
+        return BytesIO()
+
+    def splitext(self):
+        return self.name.rsplit('.', 1)
+
+    def remove(self):
+        pass
 
 
 if __name__ == "__main__":
