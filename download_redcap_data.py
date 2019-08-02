ab9d075cd0b360556f1e3f99babc022f46e2477c
Lav Patel <contact.lavpatel@gmail.com>
2019-08-02 13:28:09 -0500

    moved make_redcap_api_call and dev dependancies

--- download_redcap_data.py
+++ download_redcap_data.py
@@ -1,9 +1,38 @@
-from redcap_api import make_redcap_api_call
+import requests
 import ConfigParser
 from os import environ
 from os.path import join
 
 
+def make_redcap_api_call(redcap_api_url, data):
+    """
+
+    >>> make_redcap_api_call(redcap_api_url='https://redcap.kumc.edu/api/', data = { \
+        'content' : 'exportFieldNames' \
+        ,'format' : 'json' \
+        ,'returnFormat' : 'json' \
+        ,'field' : 'email_id' \
+        ,'token' : 'token' \
+        })
+    '[{"original_field_name":"email_id","choice_value":"","export_field_name":"email_id"}]'
+    """
+
+    try:
+        response = requests.post(redcap_api_url, data)
+        if response.status_code == 200:
+            return response.content
+        else:
+            raise Exception('%s - %s' %
+                            (response.status_code, response.content))
+
+    except Exception as e:
+        print """
+            redcap rest call was unsucessful
+            or target server is down/ check configuration
+            %s
+            """ % (e)
+
+            
 def read_config(config_file):
        
     config = ConfigParser.ConfigParser()
