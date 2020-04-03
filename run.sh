36fb4c50af0055dd335626c24dcc03e12d169bbd
Lav Patel <contact.lavpatel@gmail.com>
2020-04-03 10:19:59 -0500

    seeting up covid redcap

---
+++ run.sh
@@ -0,0 +1,3 @@
+/bin/bash setup.sh
+source .env
+python2 download_redcap_data.py 'config_env_token.ini' '16558-COVID-19-ICU-surge-capacity-survey'
---
+++ setup.sh
@@ -0,0 +1,5 @@
+mkdir -p export/attachments
+python3 -m venv env
+source env/bin/activat
+pip2 install -r requirements.txt
+python2 download_redcap_data.py 'config_env_token.ini' '16558-COVID-19-ICU-surge-capacity-survey'
