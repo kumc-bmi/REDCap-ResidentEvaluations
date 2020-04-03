10aa08fbdb13dd1120a5bb6275bfb133c43fc92d
Lav Patel <contact.lavpatel@gmail.com>
2020-04-03 10:31:29 -0500

    added helpful hint

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 set -x
 /bin/bash setup.sh
-source .env
+echo "" >>.secrets
+echo "Have you added your secret api in .secret file ??? Dont version controlled secrets"
+source .secrets
 python2 download_redcap_data.py 'config_env_token.ini' '16558-COVID-19-ICU-surge-capacity-survey'
