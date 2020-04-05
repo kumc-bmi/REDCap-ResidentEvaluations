29a14e2cf5c645658620dac231072282be26b14e
Lav Patel <contact.lavpatel@gmail.com>
2020-04-05 10:13:26 -0500

    added python3 support

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 set -x
-#/bin/bash setup.sh
+
+source env/bin/activate
+which python2
+which python3
+
 echo "" >>.secrets
 echo "Have you added your secret api in .secret file ??? Dont version controlled secrets"
 source .secrets
 python2 download_redcap_data.py 'config_env_token.ini' '16558-COVID-19-ICU-surge-capacity-survey'
+python3 covid_19_ICU_report.py
--- setup.sh
+++ setup.sh
@@ -9,5 +9,11 @@ pip2 install --upgrade pip
 pip2 install -r requirements.txt
 pip2 freeze >requirements_pip_freeze.txt
 
+pip3 install --upgrade pip
+pip3 install -r requirements_pip3.txt
+pip3 freeze >requirements_pip3_freeze.txt
+
 which python2
 which pip2
+which python3
+which pip3
